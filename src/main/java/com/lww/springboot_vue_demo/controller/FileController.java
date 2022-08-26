6cc45bd6f7a65a3ab1c1896c9fdbed9934393877
sandwichfe <1344382941@qq.com>
2022-08-26 16:40:54 +0800

    增加获取所有盘符列表

--- src/main/java/com/lww/springboot_vue_demo/controller/FileController.java
+++ src/main/java/com/lww/springboot_vue_demo/controller/FileController.java
@@ -1,6 +1,7 @@
 package com.lww.springboot_vue_demo.controller;
 
 
+import org.junit.Test;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,27 +16,57 @@ public class FileController {
 
     @PostMapping("/list1")
     @ResponseBody  //以json方式输出过去
-    public Object Hello(String path,String keyword) {
-        List<Map<String, String>> contents = getContents(path);
-        // keyword搜索
-        if (StringUtils.hasText(keyword)){
-            contents = queryByKeyWord(keyword,contents);
+    public Object Hello(String path, String keyword) {
+        List<Map<String, String>> contents;
+        //返回根路径 列出所有盘符
+        if ("root".equals(path)) {
+            contents = listDisk();
+        } else {
+            // 返回某文件/文件夹路径
+            contents = getContents(path);
+            // keyword搜索
+            if (StringUtils.hasText(keyword)) {
+                contents = queryByKeyWord(keyword, contents);
+            }
         }
         Map<String, Object> map = new HashMap<>();
         map.put("data", contents);
         return map;
     }
 
+    /**
+     * 当前主机所有磁盘 盘符
+     * @author lww
+     * @since 2022/8/26 13:26
+     */
+    @Test
+    public List<Map<String, String>> listDisk() {
+        List<Map<String, String>> ret = new ArrayList<>();
+        File[] files = File.listRoots();
+        Map<String, String> cont;
+        if (files != null && files.length > 0) {
+            for (File file : files) {
+                cont = new HashMap<>();
+                cont.put("type", "folder");
+                cont.put("path", file.toString().replace(File.separator, ""));
+                cont.put("preName", file.toString().replace(File.separator, ""));
+                System.out.println(cont);
+                ret.add(cont);
+            }
+        }
+        return ret;
+    }
 
-    private List<Map<String, String>> queryByKeyWord(String keyword,List<Map<String, String>> contents) {
-        if (contents==null||contents.size()==0){
+
+    private List<Map<String, String>> queryByKeyWord(String keyword, List<Map<String, String>> contents) {
+        if (contents == null || contents.size() == 0) {
             return null;
         }
         List<Map<String, String>> ret = new ArrayList<>();
         StringBuilder stringBuilder;
         for (Map<String, String> content : contents) {
-             stringBuilder= new StringBuilder(content.get("preName"));
-            if (stringBuilder.indexOf(keyword)!=-1){
+            stringBuilder = new StringBuilder(content.get("preName"));
+            if (stringBuilder.indexOf(keyword) != -1) {
                 ret.add(content);
             }
         }
@@ -58,13 +89,14 @@ public class FileController {
      * @Date：
      */
     public List<Map<String, String>> getContents(String path) {
-        if ("E:".equals(path)){
-            path = "E:/";
+        // 盘符根路径 处理
+        if (Arrays.asList(new String[]{"A:", "B:", "C:", "D:", "E:", "F:", "G:", "H:", "I:", "J:", "K:", "L:", "M:", "N:", "O:", "P:", "Q:", "R:", "S:", "T:", "U:", "V:", "W:", "X:", "Y:", "Z:"}).contains(path)) {
+            path = path + "/";
         }
         List<Map<String, String>> contents = new ArrayList<>();
         File file = new File(path);
         // 不存在此路径直接返回空
-        if (!file.exists()){
+        if (!file.exists()) {
             return Collections.emptyList();
         }
         File[] tempList = file.listFiles();
@@ -75,8 +107,6 @@ public class FileController {
                 cont.put("type", "file");
                 cont.put("path", tempList[i].toString());
                 cont.put("preName", tempList[i].getName());
-                //文件名，不包含路径
-                //String fileName = tempList[i].getName();
             }
             if (tempList[i].isDirectory()) {
                 cont.put("type", "folder");
@@ -90,33 +120,9 @@ public class FileController {
 
 
     /**
-     * 获取真实ip地址，避免获取代理ip
-     */
-    public static String getIpAddress(HttpServletRequest request) {
-        String ip = request.getHeader("x-forwarded-for");
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_CLIENT_IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
-        }
-        return ip;
-    }
-
-    /**
      * 获取访问用户的客户端IP（适用于公网与局域网）.
      */
-    public static final String getIpAddr(final HttpServletRequest request)
-            throws Exception {
+    public static final String getIpAddr(final HttpServletRequest request) throws Exception {
         if (request == null) {
             throw (new Exception("getIpAddr method HttpServletRequest Object is null"));
         }
@@ -150,7 +156,6 @@ public class FileController {
     public static boolean isBlank(String s) {
         return s == null ? true : s.trim().length() == 0;
     }
-
 
 
 }
